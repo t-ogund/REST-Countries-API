393dbeaa5d2b8bfc248adc263dc857553a42c9e6
Toye Ogundepo <ogundepo@gmail.com>
2022-02-28 18:48:00 -0600

    display bordering countries.

--- src/Detail.js
+++ src/Detail.js
@@ -12,20 +12,26 @@ function Detail(props) {
 let { id } = useParams();
     let currentCountry;
     currentCountry = props.countryData.filter(country => id === country.name);
+    console.log("CURRENT COUNTRY: ", currentCountry)
     let topLevelDomain = currentCountry[0].topLevelDomain.map(tld => tld)
     let currencies = currentCountry[0].currencies.map(currency => currency.name)
     let languages = currentCountry[0].languages.map(language => language.name)
+    let countriesWithBorders = props.countryData.filter(country => country.borders)
+    console.log("COUNTRIES WITH BORDERS: ", countriesWithBorders)
     let borderCountries
     
+    let newGroup
 
     if (currentCountry[0].borders) {
-        borderCountries = currentCountry[0].borders.map(borderingCountry => {
-            
-        })
-        console.log("BORDER COUNTRIES FULL: ", borderCountries)
-       
-    } else {
-        borderCountries = "N/A"
+    borderCountries = currentCountry[0].borders.map(borderingCountry => borderingCountry)
+    .map(borderingCountry => {
+        for (let i = 0; i < countriesWithBorders.length; i++) {
+            if (countriesWithBorders[i].alpha3Code.includes(borderingCountry)) {
+                console.log(countriesWithBorders[i].name, borderingCountry)
+                return <div className="border-countries">{countriesWithBorders[i].name}</div>
+            }
+        }
+    })
     }
 
     
@@ -55,8 +61,8 @@ let { id } = useParams();
                             <li><b>Languages: </b> {languages.join(", ")}</li>
                         </ul>
                     </aside>
-                    <aside>
-                        <h3>Border Countries: </h3>
+                    <aside className="border-aside">
+                        <h3 className="borders-h3">Border Countries: </h3>
                         {borderCountries}
                     </aside>
                 </section>
